a2ff6453b249f0e0b179adbe92702b7ac025d1ef
TheEryk <erykmisiek@gmail.com>
2019-05-13 22:48:33 +0200

    at8 fix

---
+++ Atmega8/Atmega8/Atmega8/src/main.c
@@ -0,0 +1,52 @@
+/*
+ * main.c
+ *
+ *  Created on: 3 lis 2018
+ *      Author: soko2
+ */
+#include <avr/io.h>
+#ifndef F_CPU
+	#define F_CPU 16000000UL
+#endif
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+#define KEY_PIN1 (1<<PIND2)
+#define KEY_DOWN1 ! (PIND & KEY_PIN1)
+
+#define KEY_PIN2 (1<<PIND3)
+#define KEY_DOWN2 ! (PIND & KEY_PIN2)
+
+
+int main (void)
+{
+	DDRD &= ~(KEY_PIN1 | KEY_PIN2); //PD2 i PD3 jako wejœcia
+	PORTD = (KEY_PIN1)|(KEY_PIN2); //podci¹gniêcie do potencja³u
+	
+	SFIOR &= ~(1<<PUD); // zerowanie bitu PUD aby podciagniecie faktycznie dzialalo
+
+	DDRB   =(1<<PINB1)|(1<<PINB2); //PB1 i PB2 jako wyjœcia
+	TCNT1 = 0;
+	TCCR1A =(1<<COM1A1)|(1<<COM1B1)|(1<<WGM10); //ustawienie FastPWM
+	TCCR1B =(1<<WGM12)|(1<<CS10)|(1<<CS11); //preskaler 64
+	TIMSK = 0; // wylaczenie przerwan
+	// 254- pocz¹tek
+	// 55- koniec
+	// 150 stopni obrót
+	
+    while (1)
+    {
+    	if (KEY_DOWN1)
+    	{
+    		OCR1A=254;
+    	}
+    	if (KEY_DOWN2)
+    	{
+    		OCR1A=190;
+    	}
+		_delay_ms(10);
+    }
+}
+
+
